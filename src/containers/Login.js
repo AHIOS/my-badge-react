e33c7cc5edfd2cda1956f82491fdc0e18ffc515b
Giuseppe Valenti <giuseppevalenti@Giuseppes-MBP-4.station>
2017-10-02 12:48:16 +0200

    * social icon

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -8,10 +8,10 @@ export default class Login extends Component {
         <div className="lander">
           <h1>Login</h1>
           <div>
-            <p><a href="/auth/linkedin" className="btn btn-social btn-linkedin">Login with LinkedIn</a></p>
-            <p><a href="/auth/github" className="btn btn-social btn-github">Login with Github</a></p>
-            <p><a href="/auth/facebook" className="btn btn-social btn-facebook">Login with Facebook</a></p>
-            <p><a href="/auth/twitter" className="btn btn-social btn-twitter">Login with Twitter</a></p>
+            <p><a href="/auth/linkedin" className="btn btn-social btn-linkedin"><span class="fa fa-linkedin"></span> Sign in with LinkedIn</a></p>
+            <p><a href="/auth/github" className="btn btn-social btn-github"><span class="fa fa-github"></span> Sign in with Github</a></p>
+            <p><a href="/auth/facebook" className="btn btn-social btn-facebook"><span class="fa fa-facebook"></span> Sign in with Facebook</a></p>
+            <p><a href="/auth/twitter" className="btn btn-social btn-twitter"><span class="fa fa-twitter"></span> Sign in with Twitter</a></p>
           </div>
         </div>
       </div>
